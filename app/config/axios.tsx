4f7673d57a702b373c4e061340f54fd0854508b8
Muhammed souban <muhammedsoubankunnummel123@gmail.com>
2024-05-10 18:21:31 +0530

    fix : err

--- app/config/axios.tsx
+++ app/config/axios.tsx
@@ -2,7 +2,8 @@ import axios from 'axios'
 import cookie from 'js-cookie'
 
 const Axios = axios.create({
-    baseURL:'http://localhost:8080/api/',
+    // baseURL:'http://localhost:8080/api/',
+    baseURL:'https://adimin-dashbord-server.onrender.com',
     headers:{
         "Content-Type":"application/json",
         Authorization: cookie.get("token")
